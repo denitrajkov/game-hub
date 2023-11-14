ced2427eb2b39a5861ff1fcac875c2f732f7564c
denitrajkov <denitrajkov@yahoo.com>
2023-11-14 09:21:52 +0100

    Displaying critic score badge

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,6 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
-
 import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
 interface Props {
