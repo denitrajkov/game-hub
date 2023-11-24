908e8c07062be132a3bb5c25ce4b9ce7a8d660ef
denitrajkov <denitrajkov@yahoo.com>
2023-11-24 13:58:28 +0100

    Some changes

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,9 +1,29 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const GameCardSkeleton = () => {
+  const [skeletonWidth, setSkeletonWidth] = useState("500px");
+
+  useEffect(() => {
+    const updateWidth = () => {
+      if (window.innerWidth < 768) {
+        setSkeletonWidth("100%");
+      } else {
+        setSkeletonWidth("500px");
+      }
+    };
+
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateWidth);
+    };
+  }, []);
+
   return (
     <Card>
-      <Skeleton height="200px" />
+      <Skeleton width={skeletonWidth} height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
